8ed59968bf671939bab5eb283af75601b1cb89a4
Artem Holodilov <art.holodilov@gmail.com>
2023-12-12 23:28:08 +0800

    Version 1

--- app/src/main/java/com/terabyte/map/excursions/json/IntroImageJson.kt
+++
@@ -1,8 +0,0 @@
-package com.terabyte.map.excursions.json
-
-import com.squareup.moshi.Json
-
-data class IntroImageJson(
-    @Json(name = "id") val id: Int,
-    @Json(name = "caption") val caption: String
-)
